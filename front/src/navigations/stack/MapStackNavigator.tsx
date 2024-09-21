907f063e6b7480963c9221194d798052f3a63ba2
dokimion24 <leecy3610@gmail.com>
2024-09-21 18:28:51 +0900

    MapStackNavigator

---
+++ front/src/navigations/stack/MapStackNavigator.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {mapNavigations} from '@/constants';
+import MapHomeScreen from '@/screens/map/MapHomeScreen';
+
+const Stack = createStackNavigator<MapStackParamList>();
+
+export type MapStackParamList = {
+  [mapNavigations.MAP_HOME]: undefined;
+};
+
+export default function MapStackNavigator() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        cardStyle: {
+          backgroundColor: 'white',
+        },
+        headerStyle: {
+          backgroundColor: 'white',
+          shadowColor: 'gray',
+        },
+        headerTitleStyle: {
+          fontSize: 15,
+        },
+        headerTintColor: 'black',
+      }}
+      children={null}>
+      <Stack.Screen
+        name={mapNavigations.MAP_HOME}
+        component={MapHomeScreen}
+        options={{
+          headerTitle: ' ',
+          headerShown: false,
+        }}
+      />
+    </Stack.Navigator>
+  );
+}

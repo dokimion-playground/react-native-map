d0b86e3aa32f4b091a61a96de727a0cb8a43e273
dokimion24 <leecy3610@gmail.com>
2024-09-22 18:33:13 +0900

    Add MapHomeScreen with user location and drawer access

--- front/src/screens/map/MapHomeScreen.tsx
+++ front/src/screens/map/MapHomeScreen.tsx
@@ -1,15 +1,59 @@
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {Button, Pressable, SafeAreaView, StyleSheet, Text} from 'react-native';
 import React from 'react';
 import useAuth from '@/hooks/queries/useAuth';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {colors} from '@/constants';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {CompositeNavigationProp, useNavigation} from '@react-navigation/native';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {MapStackParamList} from '@/navigations/stack/MapStackNavigator';
+import {DrawerNavigationProp} from '@react-navigation/drawer';
+import {MainDrawerParamList} from '@/navigations/drawer/MainDrawerNavigator';
 
-export default function MapHomeScreen() {
+type Navigation = CompositeNavigationProp<
+  StackNavigationProp<MapStackParamList>,
+  DrawerNavigationProp<MainDrawerParamList>
+>;
+
+export default function MapHomeScreen({}) {
+  const inset = useSafeAreaInsets();
+  const navigation = useNavigation<Navigation>();
+
   const {logoutMutation} = useAuth();
   return (
-    <View>
-      <Text>MapHomeScreen</Text>
-      <Button title="로그아웃" onPress={() => logoutMutation.mutate()} />
-    </View>
+    <>
+      <MapView
+        style={styles.container}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation
+        followsUserLocation
+        showsMyLocationButton={false}
+      />
+      <Pressable
+        style={[styles.drawerButton, {top: inset.top || 20}]}
+        onPress={() => navigation.openDrawer()}>
+        <Text>서랍</Text>
+      </Pressable>
+    </>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  drawerButton: {
+    position: 'absolute',
+    left: 0,
+    top: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    backgroundColor: colors.primary,
+    borderTopEndRadius: 50,
+    borderBottomEndRadius: 50,
+    shadowColor: colors.black,
+    shadowOffset: {width: 1, height: 1},
+    shadowOpacity: 0.5,
+    elevation: 4,
+  },
+});

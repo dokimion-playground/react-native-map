f2c757f156b4739e69e10729d764f0d14e55ed66
dokimion24 <leecy3610@gmail.com>
2024-09-23 20:44:35 +0900

    Add user location tracking with Geolocation

--- front/src/screens/map/MapHomeScreen.tsx
+++ front/src/screens/map/MapHomeScreen.tsx
@@ -1,7 +1,15 @@
-import {Button, Pressable, SafeAreaView, StyleSheet, Text} from 'react-native';
-import React from 'react';
+import {
+  Alert,
+  PermissionsAndroid,
+  Platform,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
 import useAuth from '@/hooks/queries/useAuth';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView, {LatLng, PROVIDER_GOOGLE} from 'react-native-maps';
 import {colors} from '@/constants';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {CompositeNavigationProp, useNavigation} from '@react-navigation/native';
@@ -9,20 +17,78 @@ import {StackNavigationProp} from '@react-navigation/stack';
 import {MapStackParamList} from '@/navigations/stack/MapStackNavigator';
 import {DrawerNavigationProp} from '@react-navigation/drawer';
 import {MainDrawerParamList} from '@/navigations/drawer/MainDrawerNavigator';
+import Geolocation from 'react-native-geolocation-service';
 
 type Navigation = CompositeNavigationProp<
   StackNavigationProp<MapStackParamList>,
   DrawerNavigationProp<MainDrawerParamList>
 >;
 
+const requestLocationPermission = async () => {
+  if (Platform.OS === 'android') {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    );
+    return granted === PermissionsAndroid.RESULTS.GRANTED;
+  }
+  return true;
+};
+
 export default function MapHomeScreen({}) {
   const inset = useSafeAreaInsets();
   const navigation = useNavigation<Navigation>();
+  const [userLocation, setUserLocation] = useState<LatLng>({
+    latitude: 37.5665,
+    longitude: 126.978,
+  });
+  const mapRef = useRef<MapView | null>(null);
 
   const {logoutMutation} = useAuth();
+
+  const handlePressUserLocation = () => {
+    mapRef.current?.animateToRegion({
+      latitude: userLocation.latitude,
+      longitude: userLocation.longitude,
+      latitudeDelta: 0.0421,
+      longitudeDelta: 0.0922,
+    });
+  };
+
+  useEffect(() => {
+    const getLocation = async () => {
+      const hasPermission = await requestLocationPermission();
+      if (!hasPermission) {
+        console.log('Location permission denied');
+        return;
+      }
+
+      const watchId = Geolocation.watchPosition(
+        position => {
+          const {latitude, longitude} = position.coords;
+          setUserLocation({latitude, longitude});
+        },
+        error => {
+          console.error('Location Error:', error);
+          Alert.alert('Error', 'Failed to get location');
+        },
+        {
+          enableHighAccuracy: true,
+          distanceFilter: 1,
+        },
+      );
+      return () => {
+        Geolocation.clearWatch(watchId);
+      };
+    };
+
+    getLocation();
+  }, []);
+  console.log('userLocation', userLocation);
+
   return (
     <>
       <MapView
+        ref={mapRef}
         style={styles.container}
         provider={PROVIDER_GOOGLE}
         showsUserLocation
@@ -34,6 +100,11 @@ export default function MapHomeScreen({}) {
         onPress={() => navigation.openDrawer()}>
         <Text>서랍</Text>
       </Pressable>
+      <View style={styles.buttonList}>
+        <Pressable style={styles.mapButton} onPress={handlePressUserLocation}>
+          <Text></Text>
+        </Pressable>
+      </View>
     </>
   );
 }
@@ -55,5 +126,22 @@ const styles = StyleSheet.create({
     shadowOffset: {width: 1, height: 1},
     shadowOpacity: 0.5,
     elevation: 4,
+  },
+  buttonList: {
+    position: 'absolute',
+    bottom: 30,
+    right: 15,
+  },
+  mapButton: {
+    backgroundColor: colors.primary,
+    marginVertical: 5,
+    height: 48,
+    width: 48,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30,
+    shadowOffset: {width: 1, height: 2},
+    shadowOpacity: 0.5,
+    elevation: 2,
   },
 });

b848b020365d2b611c8e7a2ae96f4a370753764b
dokimion24 <leecy3610@gmail.com>
2024-10-01 14:15:02 +0900

    Add addPost form

--- front/src/screens/map/AddPostScreen.tsx
+++ front/src/screens/map/AddPostScreen.tsx
@@ -1,21 +1,82 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {SafeAreaView, ScrollView, StyleSheet, View} from 'react-native';
+import React, {useEffect, useRef} from 'react';
 import {StackScreenProps} from '@react-navigation/stack';
 import {MapStackParamList} from '@/navigations/stack/MapStackNavigator';
-import {mapNavigations} from '@/constants';
+import {colors, mapNavigations} from '@/constants';
+import InputField from '@/components/InputField';
+import Octicons from 'react-native-vector-icons/Octicons';
+import CustomButton from '@/components/CustomButton';
+import useForm from '@/hooks/useForm';
+import {TextInput} from 'react-native-gesture-handler';
+import {validateAddPost} from '@/utils';
+import AddPostHeaderRight from '@/components/AddPostHeaderRight';
 
 type AddPostScreenProps = StackScreenProps<
   MapStackParamList,
   typeof mapNavigations.ADD_POST
 >;
 
-export default function AddPostScreen({route}: AddPostScreenProps) {
+export default function AddPostScreen({route, navigation}: AddPostScreenProps) {
   const {location} = route.params;
+
+  const descriptionRef = useRef<TextInput | null>(null);
+  const addPost = useForm({
+    initialValues: {
+      title: '',
+      description: '',
+    },
+    validate: validateAddPost,
+  });
+
+  const handleSubmit = () => {};
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => AddPostHeaderRight(handleSubmit),
+    });
+  });
+
   return (
-    <View>
-      <Text>AddPostScreen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.contentContainer}>
+        <View style={styles.inputContainer}>
+          <InputField
+            value=""
+            disabled
+            icon={<Octicons name="location" size={16} color={colors.grey} />}
+          />
+          <CustomButton variant="outlined" size="large" label="날짜 선택" />
+          <InputField
+            placeholder="제목을 입력하세요."
+            error={addPost.errors.title}
+            touched={addPost.touched.title}
+            returnKeyType="next"
+            blurOnSubmit={false}
+            onSubmitEditing={() => descriptionRef.current?.focus()}
+            {...addPost.getTextInputProps('title')}
+          />
+          <InputField
+            ref={descriptionRef}
+            placeholder="기록하고 싶은 내용을 입력하세요."
+            error={addPost.errors.description}
+            touched={addPost.touched.description}
+            multiline
+            returnKeyType="next"
+            {...addPost.getTextInputProps('description')}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  inputContainer: {
+    gap: 20,
+    marginBottom: 20,
+  },
+  contentContainer: {flex: 1, padding: 20, marginBottom: 10},
+});

5e7ea902c7244c4f5a81e28f7e4beaf4e2c92111
dokimion24 <leecy3610@gmail.com>
2024-10-02 14:16:51 +0900

    Add icon props

--- front/src/components/InputField.tsx
+++ front/src/components/InputField.tsx
@@ -1,4 +1,4 @@
-import React, {ForwardedRef, forwardRef, useRef} from 'react';
+import React, {ForwardedRef, forwardRef, ReactNode, useRef} from 'react';
 import {
   Dimensions,
   StyleSheet,
@@ -15,13 +15,14 @@ interface InputFieldProps extends TextInputProps {
   disabled?: boolean;
   error?: string;
   touched?: boolean;
+  icon?: ReactNode;
 }
 
 const deviceHeight = Dimensions.get('screen').height;
 
 const InputField = forwardRef(
   (
-    {disabled = false, error, touched, ...props}: InputFieldProps,
+    {disabled = false, error, touched, icon, ...props}: InputFieldProps,
     ref?: ForwardedRef<TextInput>,
   ) => {
     const innerRef = useRef<TextInput | null>(null);
@@ -36,19 +37,23 @@ const InputField = forwardRef(
           style={[
             styles.container,
             disabled && styles.disabled,
+            props.multiline && styles.multiLine,
             touched && !!error && styles.inputError,
           ]}>
-          <TextInput
-            ref={ref ? composeRefs(innerRef, ref) : innerRef}
-            editable={!disabled}
-            placeholderTextColor={colors.textDisabled}
-            autoCapitalize="none"
-            spellCheck={false}
-            autoCorrect={false}
-            style={[styles.input, disabled && styles.disabled]}
-            {...props}
-          />
-          {touched && !!error && <Text style={styles.error}>{error}</Text>}
+          <View style={Boolean(icon) && styles.innerContainer}>
+            {icon}
+            <TextInput
+              ref={ref ? composeRefs(innerRef, ref) : innerRef}
+              editable={!disabled}
+              placeholderTextColor={colors.textDisabled}
+              autoCapitalize="none"
+              spellCheck={false}
+              autoCorrect={false}
+              style={[styles.input, disabled && styles.disabled]}
+              {...props}
+            />
+            {touched && !!error && <Text style={styles.error}>{error}</Text>}
+          </View>
         </View>
       </Pressable>
     );
@@ -61,6 +66,7 @@ const styles = StyleSheet.create({
     borderColor: colors.border,
     padding: deviceHeight > 700 ? 15 : 10,
   },
+  innerContainer: {flexDirection: 'row', alignItems: 'center'},
   input: {
     fontSize: 16,
     color: colors.textPrimary,
@@ -78,6 +84,9 @@ const styles = StyleSheet.create({
     color: colors.error,
     fontSize: 12,
     paddingTop: 2,
+  },
+  multiLine: {
+    paddingBottom: deviceHeight > 700 ? 45 : 30,
   },
 });
 

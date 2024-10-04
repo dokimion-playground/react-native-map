1af76f31384bda1aa23fdf8a10082bbb23f0c9f6
dokimion24 <leecy3610@gmail.com>
2024-10-04 16:12:11 +0900

    Optimize marker cache update in onSuccess

--- front/src/hooks/queries/useMutateCreatePost.ts
+++ front/src/hooks/queries/useMutateCreatePost.ts
@@ -1,10 +1,32 @@
 import {createPost} from '@/api/post';
+import queryClient from '@/api/queryClient';
+import {queryKeys} from '@/constants';
 import {UseMutatiolnCustomOptions} from '@/types/common';
+import {Marker} from '@/types/domain';
 import {useMutation} from '@tanstack/react-query';
 
 const useMutateCreatePost = (mutationOptions?: UseMutatiolnCustomOptions) => {
   return useMutation({
     mutationFn: createPost,
+    onSuccess: newPost => {
+      // queryClient.invalidateQueries({
+      //   queryKey: [queryKeys.MARKER, queryKeys.GET_MARKERS],
+      // });
+      queryClient.setQueryData<Marker[]>(
+        [queryKeys.MARKER, queryKeys.GET_MARKERS],
+        existingMarkers => {
+          const newMarker = {
+            id: newPost.id,
+            latitude: newPost.latitude,
+            longitude: newPost.longitude,
+            score: newPost.score,
+          };
+          return existingMarkers
+            ? [...existingMarkers, newMarker]
+            : [newMarker];
+        },
+      );
+    },
     ...mutationOptions,
   });
 };

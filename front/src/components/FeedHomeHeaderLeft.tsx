19be1c4b4183cff83cfb2d48953f4b505730b011
dokimion24 <leecy3610@gmail.com>
2024-10-06 07:05:04 +0900

    Add FeedHomeHeaderLeft component

---
+++ front/src/components/FeedHomeHeaderLeft.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import HeaderButton from './HeaderButton';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {CompositeNavigationProp} from '@react-navigation/native';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {FeedStackParamList} from '@/navigations/stack/FeedStackNavigator';
+import {DrawerNavigationProp} from '@react-navigation/drawer';
+import {MainDrawerParamList} from '@/navigations/drawer/MainDrawerNavigator';
+
+type FeedHomeHeaderLeftProps = CompositeNavigationProp<
+  StackNavigationProp<FeedStackParamList>,
+  DrawerNavigationProp<MainDrawerParamList>
+>;
+
+export default function FeedHomeHeaderLeft(
+  navigation: FeedHomeHeaderLeftProps,
+) {
+  return (
+    <HeaderButton
+      icon={<Ionicons name="menu" size={25} />}
+      onPress={() => navigation.openDrawer()}
+    />
+  );
+}
